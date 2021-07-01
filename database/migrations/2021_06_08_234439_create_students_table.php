f307397e2f1b9c99f169f422856863b49bee6600
wassim.kaouia <58480846+wassim-kaouia@users.noreply.github.com>
2021-07-02 00:16:57 +0200

    version1

---
+++ database/migrations/2021_06_08_234439_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nationality')->nullable();
+            $table->integer('age');
+            $table->string('cin');
+            $table->text('address')->nullable();
+            $table->string('gsm');
+            $table->string('study_level')->nullable();
+            $table->string('photo_profile')->default('/profile_default.jpeg');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
