5378b7b226571e73e643023cef4be944d8c4aa27
wassim.kaouia <58480846+wassim-kaouia@users.noreply.github.com>
2021-08-26 13:18:14 +0200

    ok

--- database/migrations/2021_06_08_234332_create_reviews_table.php
+++ database/migrations/2021_06_08_234332_create_reviews_table.php
@@ -14,6 +14,7 @@ class CreateReviewsTable extends Migration
             $table->string('content');
             $table->integer('stars')->default(5);
             $table->foreignId('course_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
